5e5499f6abbc039760118126dab3283672c6a709
Abid Ali <117859453+Abid-Ali-01@users.noreply.github.com>
2024-12-06 00:18:32 +0500

    Add files via upload

---
+++ Easy/find_the_index_of_the_first_occurrence_in_a_string.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+       
+        int index = haystack.find(needle);
+        
+        for(int i = 0; i <= haystack.size()-1; i++){
+          if(index != string::npos){
+            return index;
+          }
+        }
+    return index;
+    }
+};
+
+
+int main() {
+    Solution s;
+int index = s.strStr("fdacascwubfuecifoundajeuecbscuasdbaasichasifas", "");
+cout<<index;
+
+    return 0;
+}
