82b6015b01f26792b02e268a93497d33476c6290
Abid Ali <117859453+Abid-Ali-01@users.noreply.github.com>
2024-12-06 01:04:39 +0500

    Add files via upload

---
+++ Easy/Majority_element.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include<cmath>
+using namespace std;
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+      int majority = nums.size()/2;
+      for(int i = 0; i<= nums.size(); i++){
+       int c = count(nums.begin(), nums.end(), nums[i]);
+  
+         if(c > majority){
+         return nums[i];
+         }
+      }
+      return 0;
+  
+    }
+};
+
+
+int main() {
+    Solution s;
+    vector<int> nums = {2,2,1,1,1,2,2};
+  int count = s.majorityElement(nums);
+  cout<<count;
+    return 0;
+}
